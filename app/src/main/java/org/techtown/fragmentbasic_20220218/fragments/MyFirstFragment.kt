134c09a02001562870516aa3af7029d8e1a353b2
집윤주 <toyaann@naver.com>
2022-02-18 15:17:32 +0900

    Fragment() 상속 + xml 끌어와서 연동

--- app/src/main/java/org/techtown/fragmentbasic_20220218/fragments/MyFirstFragment.kt
+++ app/src/main/java/org/techtown/fragmentbasic_20220218/fragments/MyFirstFragment.kt
@@ -1,4 +1,20 @@
 package org.techtown.fragmentbasic_20220218.fragments
 
-class MyFirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.techtown.fragmentbasic_20220218.R
+
+class MyFirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_first, container, false)   // xml 끌어오는 부분 외 뒷부분은 거의 변동없음.
+    }
+
 }
